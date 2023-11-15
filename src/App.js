bc5c78834f2ca90284d49f370a6ed709cdcbfda6
Hafsa Qureshi <143449939+HafsaQureshi1@users.noreply.github.com>
2023-11-15 20:58:42 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Header />
+    </div>
+  );
+}
+
+export default App;
